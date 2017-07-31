6b302bafc054d87d60e78e84de5e4c08d2ce2fdf
Zola <zolamcadie@gmail.com>
2017-07-31 22:46:55 +0000

    basic planning outline of required classes for game

---
+++ math_game.rb
@@ -0,0 +1,16 @@
+### game ###
+# defines players, turns
+# records current player
+
+### players ###
+# defines player name
+# records player score
+
+### turn ###
+# defines question, checks answer
+# alters player score
+# alters game turn
+
+
+### question ###
+# generates random question
