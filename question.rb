41a2fb6cd9d7de97b5bba7746787ebcc24c8ff90
Zola <zolamcadie@gmail.com>
2017-07-31 23:30:56 +0000

    defined question class

---
+++ question.rb
@@ -0,0 +1,22 @@
+#####  question  #####
+# generates question #
+# checks answer      #
+
+class Question
+
+  def initialize
+    @int_one = rand(20) + 1
+    @int_two = rand(20) + 1
+  end
+
+  def ask
+    puts "What is #{@int_one} plus #{@int_two}?"
+    answer = gets.chomp.to_i
+    if answer == @int_one + @int_two
+      true
+    else
+      false
+    end
+  end
+
+end
