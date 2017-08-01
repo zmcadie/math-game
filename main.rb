21201399dbabec5de52fd02e83bccd5c4634e3a7
Zola <zolamcadie@gmail.com>
2017-08-01 00:55:22 +0000

    defined main file to create new game

---
+++ main.rb
@@ -0,0 +1,13 @@
+require './player.rb'
+require './question.rb'
+require './turn.rb'
+require './game.rb'
+
+print 'Player One, enter your name: '
+player_one = gets.chomp
+puts
+print 'Player Two, enter your name: '
+player_two = gets.chomp
+puts
+
+Game.new(player_one, player_two).begin_game
